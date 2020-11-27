744633c0f566a5ba18ef64bab77cdeb1385f56bb
Jake <jake.k.hassel@gmail.com>
2020-11-27 11:50:43 -0500

    Responsive

---
+++ src/components/KonvaLayer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layer>
+    <v-circle :config="configCircle"></v-circle>
+  </v-layer>
+</template>
+
+<script>
+export default {
+  inject: ['configKonva'],
+  data() {
+    return {};
+  },
+  computed: {
+    configCircle() {
+      return {
+        x: this.configKonva.width / 2,
+        y: 200,
+        radius: 100,
+        fill: 'blue',
+        stroke: 'black',
+        strokeWidth: 0
+      };
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/components/KonvaStage.vue
+++ src/components/KonvaStage.vue
@@ -1,18 +1,31 @@
 <template>
-  <v-stage :config="configKonva">
-    <v-layer>
-      <v-circle :config="configCircle"></v-circle>
-    </v-layer>
-  </v-stage>
+  <div ref="stageParent" class="bg">
+    <v-stage :config="configKonva" ref="stage">
+      <v-layer>
+        <v-circle :config="configCircle"></v-circle>
+      </v-layer>
+      <konva-layer />
+    </v-stage>
+  </div>
 </template>
 <script>
+import KonvaLayer from './KonvaLayer.vue';
+
 export default {
   name: 'KonvaStage',
+  components: {
+    KonvaLayer
+  },
+  provide() {
+    return {
+      configKonva: this.configKonva
+    };
+  },
   data() {
     return {
       configKonva: {
-        width: 200,
-        height: 200
+        width: 400,
+        height: 400
       },
       configCircle: {
         x: 100,
@@ -22,9 +35,36 @@ export default {
         stroke: 'black',
         strokeWidth: 4
       }
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.fitStageIntoParentContainer);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.fitStageIntoParentContainer);
+  },
+  methods: {
+    fitStageIntoParentContainer() {
+      const container = this.$refs.stageParent;
+      const { width, height } = this.configKonva;
+      const stage = this.$refs.stage.getNode();
+
+      // now we need to fit stage into parent
+      const containerWidth = container.offsetWidth;
+      // to do this we need to scale the stage
+      const scale = containerWidth / width;
+
+      stage.width(width * scale);
+      stage.height(height * scale);
+      stage.scale({ x: scale, y: scale });
+      stage.draw();
     }
   }
-}
+};
 </script>
 
-<style></style>
+<style>
+.bg {
+  background-color: black;
+}
+</style>
